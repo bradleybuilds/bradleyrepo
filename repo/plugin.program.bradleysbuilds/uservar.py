7fcddbeae957c749688977ea43aa147bd652b563
BLRY12 <bradl79@comcast.net>
2026-01-03 15:44:48 -0600

    repo
    repo

---
+++ repo/plugin.program.bradleysbuilds/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://the666mafia.com/txt/maintance.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
